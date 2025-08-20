1c5dfdf0af426fce0dd59c4a929dcf057d79468b
Felicia Durni <felicia.durni@dbtlabsworkshop.com>
2025-08-20 18:50:37 +0000

    added documentation and references

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,30 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
-
+    from {{ ref('stg_customers') }}
+    --from raw.jaffle_shop.customers
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
-
+    from {{ ref('stg_orders') }}
+    --from raw.jaffle_shop.orders
 ),
 
 customer_orders as (
@@ -36,6 +42,15 @@ customer_orders as (
 
 ),
 
+ranked_customers as (
+
+    select
+        customer_id,
+        rank() over (order by number_of_orders desc) as customer_rank
+    from customer_orders
+
+),
+
 final as (
 
     select
@@ -44,11 +59,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        ranked_customers.customer_rank
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join ranked_customers using (customer_id)
 
 )
 
