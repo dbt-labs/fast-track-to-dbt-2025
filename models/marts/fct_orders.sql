505be78c653592329ef2d0a7e471e4c18dc18f97
Lilja Theodorsdottir <lilja.theodorsdottir@dbtlabsworkshop.com>
2025-08-21 13:38:35 +0000

    fdsa

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,20 +1,75 @@
-with orders as  (
-   
-   select id as order_id,
-        user_id as customer_id,
+{{
+    config(materialized='table')
+}}
+
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+
+    from {{ref("stg_customers")}}
+
+),
+
+orders as (
+
+    select
+        order_id,
+        customer_id,
         order_date,
         status
-   from raw.jaffle_shop.orders
+
+    from {{ref("stg_orders")}}
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
 
 ),
 
 final as (
 
-   select
-       orders.*
-   from orders
-   
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
+),
+
+ranked_customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    
+    from final
+
 )
 
-select * 
-from final
+select *
+from ranked_customers
