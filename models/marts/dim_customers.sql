53b12f0c3f2caf35a45ce63a70c6c98158a0f97d
Suet Wah Chu <suetwah.chu2@dbtlabsworkshop.com>
2025-08-21 13:28:05 +0000

    added tests, docs, updated_dim_customers to include a rank, added sourcr & ref marco codes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,23 @@
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
+    
+    from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
 
 ),
 
@@ -50,7 +50,22 @@ final as (
 
     left join customer_orders using (customer_id)
 
+),
+
+ranked_customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    from final
+
 )
 
-select * 
-from final
+select *
+from ranked_customers
+order by customer_rank
