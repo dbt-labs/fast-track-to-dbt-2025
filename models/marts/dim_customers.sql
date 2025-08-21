37b7d8421a76c900504cda4c910c65aa6796df1d
Ritika Gupta <ritika.gupta@dbtlabsworkshop.com>
2025-08-21 13:10:12 +0000

    created basic models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,24 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
+    from {{ ref('stg_customers') }}
 
-    from raw.jaffle_shop.customers
+    --from raw.jaffle_shop.customers
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
+    --from raw.jaffle_shop.orders
 
 ),
 
