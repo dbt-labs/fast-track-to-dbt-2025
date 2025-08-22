433ce61c383b2005367fb71b5941eb290cad5e06
Marcia Bastos <marcia.bastos5@dbtlabsworkshop.com>
2025-08-22 16:14:43 +0000

    add docs and source ref

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -3,13 +3,15 @@
         materialized='table'
     )
 }}
+
 with orders as  (
-   
-   select id as order_id,
-        user_id as customer_id,
+  
+   select order_id,
+        customer_id,
         order_date,
         status
-   from raw.jaffle_shop.orders
+   from {{ ref('stg_orders') }}
+   -- from raw.jaffle_shop.orders
 
 ),
 
