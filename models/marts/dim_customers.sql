1cbd0d7ab2b9ceccb649924f81ec030637ddb362
Jerrick Ng <jerrick.ng@dbtlabsworkshop.com>
2025-08-27 03:54:39 +0000

    minor changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,28 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
-
+    
+    from {{ ref('stg_customers') }}
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    
+    from {{ ref('stg_orders') }}
 
 ),
 
@@ -25,13 +30,10 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
     group by 1
 
 ),
