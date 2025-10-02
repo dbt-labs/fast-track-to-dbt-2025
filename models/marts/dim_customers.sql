dd363710d321936fbe034b67f6150675b01e32b3
uttpesh vyas <uttpesh.vyas@dbtlabsworkshop.com>
2025-10-02 13:27:37 +0000

    test

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,56 +1,37 @@
-with customers as (
+{{
+    config(
+        materialized='table'
+    )
+}}
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from raw.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
+with orders as (
+    select 
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
-
+    from {{ ref('stg_orders') }}
 ),
 
-customer_orders as (
-
+customer_order_counts as (
     select
         customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as total_orders
     from orders
-
-    group by 1
-
+    group by customer_id
 ),
 
-final as (
-
+ranked_customers as (
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
+        customer_id,
+        total_orders,
+        rank() over (order by total_orders desc) as customer_rank
+    from customer_order_counts
 )
 
-select * 
-from final
+select 
+    customer_id,
+    total_orders,
+    customer_rank
+from ranked_customers
