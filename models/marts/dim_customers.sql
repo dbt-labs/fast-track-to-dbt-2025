9863ebb97649d7e596f194cb82b7389660d37188
przemyslaw rybicki <przemyslaw.rybicki@dbtlabsworkshop.com>
2025-10-02 13:28:10 +0000

    added tests, dosc, updated some code

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
@@ -5,19 +11,20 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
-
+    --from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    --from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
 
 ),
 
