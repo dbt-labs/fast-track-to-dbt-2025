814e99da9b23bac28d68f6575e22043dbfbf473d
brian hammond <brian.hammond+1759425466@dbtlabsworkshop.com>
2025-10-02 18:28:01 +0000

    lots of changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
-
+    from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
@@ -36,6 +41,16 @@ customer_orders as (
 
 ),
 
+ranked_customers as (
+
+    select
+        customer_id,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    from customer_orders
+
+),
+
 final as (
 
     select
@@ -44,11 +59,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        ranked_customers.customer_rank
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join ranked_customers using (customer_id)
 
 )
 
