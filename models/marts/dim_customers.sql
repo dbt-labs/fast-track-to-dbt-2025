48cf0591b6a35bc9bbbe8b3b856d3f75a552027e
saaim kh <saaim.kh@dbtlabsworkshop.com>
2025-10-02 13:28:15 +0000

    Did alot!

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,12 @@
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
+    -- from raw.jaffle_shop.customers
 
 ),
 
@@ -44,7 +45,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        rank() over (order by customer_orders.number_of_orders desc) as customer_rank
 
     from customers
 
