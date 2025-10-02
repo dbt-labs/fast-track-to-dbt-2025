af75c4f7932fa0ccb2c028d076d5b2efe7a89f9e
anael bourgneuf <anael.bourgneuf@dbtlabsworkshop.com>
2025-10-02 18:27:48 +0000

    tests

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,29 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
 
 ),
 
@@ -30,25 +36,33 @@ customer_orders as (
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
-    from orders
+    from {{ ref('stg_orders') }}
 
     group by 1
+
+),
+
+ranked_customers as (
+
+    select
+        customer_id,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    from customer_orders
 
 ),
 
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.number_of_orders,
+        rc.customer_rank
+    from customers c
+    inner join customer_orders co on c.customer_id = co.customer_id
+    inner join ranked_customers rc on c.customer_id = rc.customer_id
 
 )
 
