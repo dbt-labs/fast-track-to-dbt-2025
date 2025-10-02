7738c87f28c665e1311dd0aed36e67c7d3bf36e2
jan vijvers <jan.vijvers@dbtlabsworkshop.com>
2025-10-02 13:27:50 +0000

    add test docs etc

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,56 +1,49 @@
-with customers as (
+{{ config(materialized="table") }}
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+with
+    customers as (
 
-    from raw.jaffle_shop.customers
+        select customer_id, first_name, last_name from {{ ref("stg_customers") }}
 
-),
+    ),
 
-orders as (
+    orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        select order_id,  customer_id, order_date, status
+        from {{ ref("stg_orders") }}
 
-    from raw.jaffle_shop.orders
+    ),
 
-),
+    customer_orders as (
 
-customer_orders as (
+        select customer_id, count(order_id) as number_of_orders from orders group by 1
 
-    select
-        customer_id,
+    ),
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+    ranked_customers as (
 
-    from orders
+        select
+            customer_id,
+            number_of_orders,
+            rank() over (order by number_of_orders desc) as customer_rank
+        from customer_orders
 
-    group by 1
+    ),
 
-),
+    final as (
 
-final as (
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            coalesce(ranked_customers.customer_rank, 0) as customer_rank
+        from customers
+        left join customer_orders using (customer_id)
+        left join ranked_customers using (customer_id)
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+    )
 
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * 
+select *
 from final
+order by customer_rank
