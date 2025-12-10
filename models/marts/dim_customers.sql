b232e1667c9e3f6291589db9b91ec1ee31016f69
n brock <n.brock+1765390875@dbtlabsworkshop.com>
2025-12-10 18:50:28 +0000

    parameterize ref in marts

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,56 +1,57 @@
-with customers as (
+{{ config(materialized="table") }}
 
-    select
-        id as customer_id,
-        first_name,
+with
+    customers as (
+
+        select 
+        customer_id, 
+        first_name, 
         last_name
+        -- from raw.jaffle_shop.customers
+        from {{ ref("stg_customers") }}
 
-    from raw.jaffle_shop.customers
+    ),
 
-),
+    orders as (
 
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
+        select 
+        order_id, 
+        customer_id, 
+        order_date, 
         status
+        -- from raw.jaffle_shop.orders
+        from {{ ref("stg_orders") }}
+    ),
 
-    from raw.jaffle_shop.orders
+    customer_orders as (
 
-),
+        select
+            customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
+        -- from orders
+        from {{ ref("stg_orders") }}
 
-customer_orders as (
+        group by 1
 
-    select
-        customer_id,
+    ),
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+    final as (
 
-    from orders
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    group by 1
+        from customers
 
-),
+        left join customer_orders using (customer_id)
 
-final as (
+    )
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * 
+select *
 from final
