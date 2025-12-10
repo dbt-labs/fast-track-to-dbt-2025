97d9d52d497fcce175d0aa622d7c810016b3e8cb
chip prosser <chip.prosser@dbtlabsworkshop.com>
2025-12-10 19:15:11 +0000

    swap hard coded objects w/ aliased versions

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,7 @@
 select
     id as customer_id,
     first_name,
-    last_name
-
-from raw.jaffle_shop.customers
+    last_name,
+    concat(first_name, ' ', last_name) AS full_name
+-- from raw.jaffle_shop.customers // rid of hard coded db.schema.table with shortcut
+from {{ source('jaffle_shop', 'customers') }}
