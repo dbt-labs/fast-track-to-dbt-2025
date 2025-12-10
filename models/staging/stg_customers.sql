74e7e0ff793c1ed0f2299107537c30987d5f6469
chip prosser <chip.prosser@dbtlabsworkshop.com>
2025-12-10 19:34:57 +0000

    stg_cust change

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,6 +2,6 @@ select
     id as customer_id,
     first_name,
     last_name,
-    concat(first_name, ' ', last_name) AS full_name
+    concat(first_name, ' ', last_name) as full_name
 -- from raw.jaffle_shop.customers // rid of hard coded db.schema.table with shortcut
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source("jaffle_shop", "customers") }}
