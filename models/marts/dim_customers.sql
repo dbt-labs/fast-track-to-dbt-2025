fa52766a07a08219e66b98d03fad3a2d07905c4f
jessica kavanagh <jessica.kavanagh@dbtlabsworkshop.com>
2025-12-10 19:14:28 +0000

    added tests on sources and added source for jaffle shop data

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -11,7 +11,6 @@ with customers as (
         first_name,
         last_name
 
-    --from raw.jaffle_shop.customers
     from {{ ref('stg_customers') }}
 
 ),
@@ -43,6 +42,17 @@ customer_orders as (
 
 ),
 
+ranked_customers as (
+
+    select
+        customer_id,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    
+    from customer_orders
+
+),
+
 final as (
 
     select
@@ -51,13 +61,16 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        ranked_customers.customer_rank
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join ranked_customers using (customer_id)
 
 )
 
 select * 
 from final
+order by customer_rank
