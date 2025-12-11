1c7b095035a2cd1a4a933bdf424338f16b04a536
faith khan <faith.khan+1765458935@dbtlabsworkshop.com>
2025-12-11 13:48:12 +0000

    update customers to use staging instead of hardcoded transformations

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,17 @@
+ {{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 
 ),
 
@@ -17,7 +23,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_orders') }}
 
 ),
 
