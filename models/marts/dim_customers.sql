2da50e5ef4eb267dc5589c9d0e8b91b3dc00712e
martin reid <martin.reid@dbtlabsworkshop.com>
2025-12-11 13:44:52 +0000

    add ref macro to marts models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,12 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    --from raw.jaffle_shop.customers
+    from {{ ref('stg_customers') }}
 
 ),
 
