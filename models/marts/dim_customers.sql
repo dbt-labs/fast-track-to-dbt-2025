de0030ac6a796aed5f95c8ca403eddc01814687e
gabrielle cristine <gabrielle.cristine@dbtlabsworkshop.com>
2025-12-11 13:52:45 +0000

    primeiras mudanças

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,12 +1,17 @@
+{{
+    config (
+        materialized='table'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
-
+    from {{ ref('stg_customers') }}
 ),
 
 orders as (
@@ -17,7 +22,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from ref
 
 ),
 
