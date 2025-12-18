5cfc40b4e6c4ff4d7566d08da2c1de24871a77c1
shay davidovitch <shay.davidovitch+1766044274@dbtlabsworkshop.com>
2025-12-18 11:56:01 +0000

    added ref macro to marts models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,56 +1,62 @@
-with customers as (
+{{ config(
+    materialized='table'
+    ) 
+}}
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+with
+    customers as (
 
-    from raw.jaffle_shop.customers
+        select 
+            customer_id, 
+            first_name, 
+            last_name 
 
-),
+        from {{ ref('stg_customers') }}
 
-orders as (
+    ),
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+    orders as (
 
-    from raw.jaffle_shop.orders
+        select 
+            order_id, 
+            customer_id, 
+            order_date, 
+            status
 
-),
+        from {{ ref('stg_orders') }}
 
-customer_orders as (
+    ),
 
-    select
-        customer_id,
+    customer_orders as (
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        select
+            customer_id,
 
-    from orders
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    group by 1
+        from orders
 
-),
+        group by 1
 
-final as (
+    ),
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+    final as (
 
-    from customers
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    left join customer_orders using (customer_id)
+        from customers
 
-)
+        left join customer_orders using (customer_id)
 
-select * 
+    )
+
+select *
 from final
