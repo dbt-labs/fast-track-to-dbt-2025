4379060af71c065125907e7d9bdf42ec2c649ae9
jenna bushspies <jenna.bushspies+1768403540@dbtlabsworkshop.com>
2026-01-14 16:46:39 +0000

    added full_name on dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -42,6 +42,7 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        customers.first_name || ' ' || customers.last_name as full_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
