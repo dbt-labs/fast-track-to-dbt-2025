012cc77f66dda3a617bd433c0441dd951628a109
mohan sharma <mohan.sharma@dbtlabsworkshop.com>
2026-02-04 19:41:58 +0000

    test commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,38 +1,38 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
-customer_orders as (
+cancelled_orders as (
 
     select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        customer_id
     from orders
-
-    group by 1
+    where status = 'cancelled'
 
 ),
 
@@ -41,14 +41,9 @@ final as (
     select
         customers.customer_id,
         customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        customers.last_name
     from customers
-
-    left join customer_orders using (customer_id)
+    inner join cancelled_orders using (customer_id)
 
 )
 
