e40fafbde9bbde7d6e1a9f900baff9a02f53469b
carson ongei <carson.ongei@dbtlabsworkshop.com>
2026-02-04 19:44:26 +0000

    carsons

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -25,7 +31,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
@@ -48,9 +53,24 @@ final as (
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join customer_orders on customers.customer_id = customer_orders.customer_id
+
+),
+
+ranked_customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders,
+        rank() over (order by number_of_orders desc) as customer_rank
+    
+    from final
 
 )
 
-select * 
-from final
+select *
+from ranked_customers
