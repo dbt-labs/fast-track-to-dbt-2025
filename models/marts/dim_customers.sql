112605b34217d24d19a1c48734abcd69bcf2103b
navaneeth narayanaswamy <navaneeth.narayanaswamy+1770228965@dbtlabsworkshop.com>
2026-02-04 19:26:59 +0000

    test v1

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -7,21 +7,22 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-    from {{ ref('stg_customers') }}
+    from {{ ref('stg_jaffle_shop__customers') }}
+
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from {{ ref('stg_orders') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -56,5 +57,5 @@ final as (
 
 )
 
-select * 
+select *
 from final
