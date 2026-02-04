cd10ad7899308d6864fafa1d161404e9c527d78e
ashrita b <ashrita.b@dbtlabsworkshop.com>
2026-02-04 19:42:13 +0000

    Adding staging models, test and doc

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -50,7 +56,16 @@ final as (
 
     left join customer_orders using (customer_id)
 
+),
+
+ranked_customers as (
+
+    select
+        *,
+        row_number() over (order by number_of_orders desc) as customer_rank
+    from final
+
 )
 
-select * 
-from final
+select *
+from ranked_customers
