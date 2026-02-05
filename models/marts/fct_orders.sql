33a380b9934f7e7b06e839ef810aa2e806be9339
joel teixeira <joel.teixeira@dbtlabsworkshop.com>
2026-02-05 14:42:09 +0000

    added test and docs

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,9 +1,9 @@
 with orders as  (
-   select id as order_id,
-        user_id as customer_id,
+   select order_id,
+        customer_id,
         order_date,
         status
-   from {{ ref("stg_orders") }}
+   from {{ ref("stg_jaffle_shop__orders") }}
 ),
 final as (
    select
