1e649e2b13671c3ea4645560426068b6f2c377ef
ron rabakukk <ron.rabakukk@dbtlabsworkshop.com>
2026-02-05 14:41:59 +0000

    Added staging models, tests and docs

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,10 +1,10 @@
 with orders as  (
    
-   select id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-   from raw.jaffle_shop.orders
+   select order_id,
+          customer_id,
+          order_date,
+          status
+   from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
