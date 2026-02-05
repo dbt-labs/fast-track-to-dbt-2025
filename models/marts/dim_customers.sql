6d38bed42077eea51af3f16164d671ec082a51d9
dennis hinken <dennis.hinken@dbtlabsworkshop.com>
2026-02-05 14:50:59 +0000

    Initial commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -52,5 +58,7 @@ final as (
 
 )
 
-select * 
+select 
+    *,
+    row_number() over (order by number_of_orders desc) as customer_rank
 from final
