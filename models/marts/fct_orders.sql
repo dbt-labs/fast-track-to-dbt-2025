e0ea7ba86113ea8f3b852fe25d9c4a4091cfed08
paula siela <paula.siela@dbtlabsworkshop.com>
2026-02-05 14:50:19 +0000

    add

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,11 +1,10 @@
 with orders as  (
    
-   select id,
-        user_id,
+   select order_id,
+        customer_id,
         order_date,
         status
    from {{ ref('stg_jaffle_shop__orders') }}
-
 ),
 
 final as (
