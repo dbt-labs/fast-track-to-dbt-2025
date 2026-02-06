c680b48991c409a395cb6774c651092b24e89620
asami tamaru <asami.tamaru@dbtlabsworkshop.com>
2026-02-06 07:39:07 +0000

    2nd commit

---
+++ models/light_customer_analytics.sql
@@ -0,0 +1,23 @@
+{{
+config(
+materialized='table',
+tags=['light', 'analytics']
+)
+}}
+/*
+軽量な顧客分析モデル
+上流: int_light_metrics (中間層)
+SAO デモ用: 上流変更で毎回ビルドされる
+*/
+select
+customer_id,
+first_name,
+last_name,
+number_of_orders,
+case
+when number_of_orders >= 3 then 'loyal'
+when number_of_orders >= 1 then 'regular'
+else 'new'
+end as customer_segment,
+current_timestamp as processed_at
+from {{ ref('int_customer_light_metrics') }}
