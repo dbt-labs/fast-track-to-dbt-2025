16bbcb20cd034f0c33fee43f03fc604363c83b97
ajay koduri <ajay.koduri@dbtlabsworkshop.com>
2026-02-17 00:09:40 +0000

    stg_player

---
+++ models/staging/fifa/stg_player.sql
@@ -0,0 +1,30 @@
+with 
+
+source as (
+
+    select * from {{ source('fifa', 'player') }}
+
+),
+
+renamed as (
+
+    select
+        id as player_id,
+        player_first_name,
+        player_middle_name,
+        player_last_name,
+        player_known_name,
+        concat(player_first_name,' ', player_last_name) as player_name,
+        birth_date,
+        --datediff('2018-06-14', DATE, birth_date)) as age,
+        weight,
+        height,
+        city,
+        national_team_affiliation_id,
+        affiliation_id
+
+    from source
+
+)
+
+select * from renamed
