3f851c6afc27e9f02150da86c0d6c72f79cb2980
robin price <robin.price+1775582643@dbtlabsworkshop.com>
2026-04-07 18:54:58 +0000

    fast track to dbt 2025

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized: "table"
+    )
+}}
+
 with customers as (
 
     select
@@ -5,7 +11,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
@@ -17,7 +23,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('_jaffle_shop_orders') }}
 
 ),
 
@@ -50,7 +56,16 @@ final as (
 
     left join customer_orders using (customer_id)
 
+),
+
+ranked_customers as (
+
+    select
+        *,
+        row_number() over (order by number_of_orders desc) as customer_rank
+    from final
+
 )
 
-select * 
-from final
+select *
+from ranked_customers
