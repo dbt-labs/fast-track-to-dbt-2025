6b4586ab7d6b039ad80f6229f078ba27151cb560
samir patel <samir.patel@dbtlabsworkshop.com>
2026-04-07 18:41:59 +0000

    added staging model, test and doc

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _ETL_LOADED_AT
+
+    from source
+
+)
+
+select * from renamed
