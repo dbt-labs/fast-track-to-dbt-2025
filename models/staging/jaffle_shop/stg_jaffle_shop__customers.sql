891830a17d01129a57625e04476c71387dd628c6
susmita pattnaik <susmita.pattnaik+1775582112@dbtlabsworkshop.com>
2026-04-07 18:42:23 +0000

    added stage, tests

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
