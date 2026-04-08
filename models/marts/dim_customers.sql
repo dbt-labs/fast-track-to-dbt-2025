c7c6f22817678217d080f7f829f6a6f17b936b23
matthew palmer-smith <matthew.palmer-smith+1775650520@dbtlabsworkshop.com>
2026-04-08 13:42:50 +0000

    Updated model

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,56 +1,61 @@
+{{ 
+    config(
+        materialized="view"
+        ) 
+}}
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select 
+            customer_id,
+            first_name,
+            last_name
 
-    from raw.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+        from {{ ref('stg_jaffle_shop__customers')}}
 
 ),
 
-customer_orders as (
+    orders as (
 
-    select
-        customer_id,
+        select 
+            order_id,
+            customer_id,
+            order_date,
+            status
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        from {{ ref('stg_jaffle_shop__orders') }}
 
-    from orders
+    ),
 
-    group by 1
+    customer_orders as (
 
-),
+        select
+            customer_id,
 
-final as (
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        from orders
 
-    from customers
+        group by 1
 
-    left join customer_orders using (customer_id)
+    ),
 
-)
+    final as (
 
-select * 
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+        from customers
+
+        left join customer_orders using (customer_id)
+
+    )
+
+select *
 from final
--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,3 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-
+select 
+    *
 from raw.jaffle_shop.customers
