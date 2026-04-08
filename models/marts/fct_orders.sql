374b3ee4488530f894ef8c74ebb25edfaca0502f
christopher spooner <christopher.spooner@dbtlabsworkshop.com>
2026-04-08 04:42:10 +0000

    added staging models, tests

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,18 +1,18 @@
 with orders as  (
    
-   select id as order_id,
-        user_id as customer_id,
+   select order_id,
+        customer_id,
         order_date,
         status
-   from raw.jaffle_shop.orders
+   from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
 final as (
 
    select
-       orders.*
-   from orders
+       *
+   from {{ ref('stg_jaffle_shop__orders') }}
    
 )
 
