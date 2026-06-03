fdb0b3e9fcfc4eaef1c7c9011251598b3e765413
matthew simanton <matthew.simanton@dbtlabsworkshop.com>
2026-06-03 18:42:33 +0000

    Testing the committ

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,29 @@
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders
+    from {{ ref('dim_customers') }}
+
+),
+
+final as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders,
+        dense_rank() over (order by number_of_orders desc, customer_id) as customer_order_rank
+    from customers
+
+)
+
+select *
+from final
