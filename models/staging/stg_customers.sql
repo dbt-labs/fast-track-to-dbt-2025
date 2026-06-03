0613536059d225b0e47831f00f03fd97935314ea
cody carroll <cody.carroll@dbtlabsworkshop.com>
2026-06-03 17:46:56 +0000

    commit

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -4,31 +4,3 @@ select
     last_name
 
 from raw.jaffle_shop.customers
-
-Save
-123456
-
-
-
-
-
-dbt Wizard
-Commands
-Results
-Code quality
-Compiled code
-Lineage$0
-
-Save
-123456
-
-
-
-
-
-dbt Wizard
-Commands
-Results
-Code quality
-Compiled code
-Lineage$0
