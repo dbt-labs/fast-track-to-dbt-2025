8dca5954ea0feccf0b33216b45b946856c4bcfcd
safa fennia <safa.fennia@dbtlabsworkshop.com>
2026-06-04 13:23:59 +0000

    first commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,28 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -44,7 +49,10 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        rank() over (
+            order by customer_orders.number_of_orders desc, customers.customer_id
+        ) as customer_rank
 
     from customers
 
@@ -52,5 +60,5 @@ final as (
 
 )
 
-select * 
+select *
 from final
