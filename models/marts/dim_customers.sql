c43ffb1ac2be1661a8c4bff719e477ac8eb81ee6
daniel powell <daniel.powell@dbtlabsworkshop.com>
2026-06-04 13:42:47 +0000

    not finished

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,11 +1,18 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    --from raw.jaffle_shop.customers
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
@@ -17,7 +24,8 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    --from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
