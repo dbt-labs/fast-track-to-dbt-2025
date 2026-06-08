891ee102520a2e654dccfee18a1b0af02e574c00
karthickkumar mylsamy <karthickkumar.mylsamy@dbtlabsworkshop.com>
2026-06-08 05:25:00 +0000

    added staging models tests and docs

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,25 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
-
-    from raw.jaffle_shop.customers
-
+from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
-
-    from raw.jaffle_shop.orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -49,6 +51,15 @@ final as (
     from customers
 
     left join customer_orders using (customer_id)
+
+),
+
+ranked_customer as (
+
+    select
+        *,
+        dense_rank() over (order by number_of_orders desc) as customer_order_rank
+    from final
 
 )
 
