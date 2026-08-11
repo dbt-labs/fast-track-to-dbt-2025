d3204150a622a6170675719e4729ba11f8bf3254
rohan mathew <rohan.mathew@dbtlabsworkshop.com>
2026-08-11 18:01:49 +0000

    create customer_order_rankings_model. replace hard coded

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,37 @@
+with order_counts as (
+
+    select
+        customer_id,
+        count(order_id) as total_orders
+
+    from {{ ref('fct_orders') }}
+
+    group by 1
+
+),
+
+customer_order_rankings as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        coalesce(order_counts.total_orders, 0) as total_orders
+
+    from {{ ref('dim_customers') }} as customers
+
+    left join order_counts
+        on customers.customer_id = order_counts.customer_id
+
+)
+
+select
+    customer_id,
+    first_name,
+    last_name,
+    total_orders,
+    dense_rank() over (
+        order by total_orders desc
+    ) as customer_order_rank
+
+from customer_order_rankings
