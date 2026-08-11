42fc14656cbab909b2ea0cef506f013d5d310d45
lovely upadhayay <lovely.upadhayay@dbtlabsworkshop.com>
2026-08-11 18:01:39 +0000

    created customer_orders_ranking model and added ref

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,52 @@
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+
+    from {{ ref('dim_customers') }}
+
+),
+
+orders as (
+
+    select
+        order_id,
+        customer_id
+
+    from {{ ref('fct_orders') }}
+
+),
+
+customer_order_totals as (
+
+    select
+        customer_id,
+        count(order_id) as total_number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        coalesce(customer_order_totals.total_number_of_orders, 0) as total_number_of_orders,
+        dense_rank() over (
+            order by coalesce(customer_order_totals.total_number_of_orders, 0) desc
+        ) as customer_order_rank
+
+    from customers
+
+    left join customer_order_totals using (customer_id)
+
+)
+
+select *
+from final
