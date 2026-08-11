8870694bee5715981f4f86579c80d22131a256df
saisriram thumma <saisriram.thumma@dbtlabsworkshop.com>
2026-08-11 18:01:53 +0000

    created customer_order_rankings model and also replaced hard coded reference in dim_customer with reff macro

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,52 @@
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+
+    from {{ ref('dim_customers') }}
+
+),
+
+orders as (
+
+    select
+        order_id,
+        customer_id
+
+    from {{ ref('fct_orders') }}
+
+),
+
+customer_order_counts as (
+
+    select
+        customer_id,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        coalesce(customer_order_counts.number_of_orders, 0) as number_of_orders,
+        rank() over (
+            order by coalesce(customer_order_counts.number_of_orders, 0) desc
+        ) as customer_order_rank
+
+    from customers
+
+    left join customer_order_counts using (customer_id)
+
+)
+
+select *
+from final
