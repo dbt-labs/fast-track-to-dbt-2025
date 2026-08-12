c05e4e1e5c04607e66a4f23b2b61a0cb1f8c7bfe
gad horn <gad.horn@dbtlabsworkshop.com>
2026-08-12 13:01:51 +0000

    create customer_order_ranking model. replace hardcoded with ref macro

---
+++ models/marts/customer_order_ranking.sql
@@ -0,0 +1,46 @@
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+    from {{ ref('dim_customers') }}
+
+),
+
+orders as (
+
+    select
+        customer_id,
+        order_id
+    from {{ ref('fct_orders') }}
+
+),
+
+customer_order_totals as (
+
+    select
+        customer_id,
+        count(order_id) as total_orders
+    from orders
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        coalesce(customer_order_totals.total_orders, 0) as total_orders,
+        dense_rank() over (
+            order by coalesce(customer_order_totals.total_orders, 0) desc
+        ) as customer_order_rank
+    from customers
+    left join customer_order_totals using (customer_id)
+
+)
+
+select *
+from final
