5914b417af2b3b7730d69b782ecab63e8f36e6c1
manuel kuhn <manuel.kuhn@dbtlabsworkshop.com>
2026-08-12 13:01:46 +0000

    Created customer_order_rankings model. Replaced hard-coded references in dim_customers with ref macro.

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,42 @@
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+
+    from {{ ref('dim_customers') }}
+
+),
+
+customer_order_counts as (
+
+    select
+        customer_id,
+        count(order_id) as number_of_orders
+
+    from {{ ref('fct_orders') }}
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        coalesce(customer_order_counts.number_of_orders, 0) as number_of_orders,
+        dense_rank() over (
+            order by coalesce(customer_order_counts.number_of_orders, 0) desc
+        ) as customer_order_rank
+
+    from customers
+
+    left join customer_order_counts using (customer_id)
+
+)
+
+select *
+from final
