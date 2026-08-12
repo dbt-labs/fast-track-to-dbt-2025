c813e5352a5d79be1f30e27ef26f2b79c3761010
christian tieke <christian.tieke@dbtlabsworkshop.com>
2026-08-12 13:02:06 +0000

    new customer order rankings

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,28 @@
+with customer_order_totals as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        count(orders.order_id) as total_orders
+
+    from {{ ref('dim_customers') }} as customers
+
+    left join {{ ref('fct_orders') }} as orders using (customer_id)
+
+    group by 1, 2, 3
+
+),
+
+final as (
+
+    select
+        *,
+        dense_rank() over (order by total_orders desc) as customer_order_rank
+
+    from customer_order_totals
+
+)
+
+select *
+from final
