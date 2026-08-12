9b1a846ba25b3545e3eea10b93ebd9d14599ffb9
antigoni georgiou <antigoni.georgiou+1786536747@dbtlabsworkshop.com>
2026-08-12 13:02:01 +0000

    created customer_order_rankings model. replaced hard coded references in dim_customer with ref marco

---
+++ models/marts/customer_order_rankings.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        number_of_orders
+
+    from {{ ref('dim_customers') }}
+
+),
+
+final as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        number_of_orders,
+        dense_rank() over (
+            order by number_of_orders desc
+        ) as customer_order_rank
+
+    from customers
+
+)
+
+select *
+from final
+order by number_of_orders desc, customer_id
