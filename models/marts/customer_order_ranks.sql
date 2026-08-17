d3b3a2598726c02753bcf052e26b037654c7761a
scott hall <scott.hall@dbtlabsworkshop.com>
2026-08-17 17:20:08 +0000

    created customer_order_rankings model and replaced hard coded reference in dim_customers with ref macro

---
+++ models/marts/customer_order_ranks.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with customers as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        number_of_orders
+
+    from {{ ref('dim_customers') }}
+
+),
+
+final as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        number_of_orders,
+        dense_rank() over (
+            order by number_of_orders desc
+        ) as customer_order_rank
+
+    from customers
+
+)
+
+select *
+from final
